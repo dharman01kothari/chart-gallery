4582508f7bbf247ca5fe1ace8fdec4542497e870
dharman01kothari <65413671+dharman01kothari@users.noreply.github.com>
2020-06-04 19:04:59 -0400

    Initial
    first

---
+++ three_charts.py
@@ -0,0 +1,75 @@
+# three_charts.py
+
+#
+# CHART 1 (PIE)
+#
+#import matplotlib.pyplot as plt 
+import plotly
+import plotly.graph_objs as go
+
+
+pie_data = [
+    {"company": "Company X", "market_share": 0.55},
+    {"company": "Company Y", "market_share": 0.30},
+    {"company": "Company Z", "market_share": 0.15}
+]
+
+labels=[]
+for i in pie_data:
+    labels.append(i['company'])
+
+mkt_share=[]
+for i in pie_data:
+    mkt_share.append(i['market_share'])
+
+""" fig1, ax1 = plt.subplots()
+ax1.pie(mkt_share, labels= labels, autopct='%1.1f%%', shadow=True, startangle=90)
+ax1.axis('equal')
+
+plt.show()
+ """
+
+trace = go.Pie(labels=labels, values = mkt_share)
+plotly.offline.plot([trace], filename='pie_chart.html', auto_open= True)
+
+
+print("----------------")
+print("GENERATING PIE CHART...")
+print(pie_data) # TODO: create a pie chart based on the pie_data
+
+#
+# CHART 2 (LINE)
+#
+
+line_data = [
+    {"date": "2019-01-01", "stock_price_usd": 100.00},
+    {"date": "2019-01-02", "stock_price_usd": 101.01},
+    {"date": "2019-01-03", "stock_price_usd": 120.20},
+    {"date": "2019-01-04", "stock_price_usd": 107.07},
+    {"date": "2019-01-05", "stock_price_usd": 142.42},
+    {"date": "2019-01-06", "stock_price_usd": 135.35},
+    {"date": "2019-01-07", "stock_price_usd": 160.60},
+    {"date": "2019-01-08", "stock_price_usd": 162.62},
+]
+
+print("----------------")
+print("GENERATING LINE GRAPH...")
+print(line_data) # TODO: create a line graph based on the line_data
+
+#
+# CHART 3 (HORIZONTAL BAR)
+#
+
+bar_data = [
+    {"genre": "Thriller", "viewers": 123456},
+    {"genre": "Mystery", "viewers": 234567},
+    {"genre": "Sci-Fi", "viewers": 987654},
+    {"genre": "Fantasy", "viewers": 876543},
+    {"genre": "Documentary", "viewers": 283105},
+    {"genre": "Action", "viewers": 544099},
+    {"genre": "Romantic Comedy", "viewers": 121212}
+]
+
+print("----------------")
+print("GENERATING BAR CHART...")
+print(bar_data) # TODO: create a horizontal bar chart based on the bar_data
